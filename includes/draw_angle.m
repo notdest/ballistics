2bebe778833dbd33d85e4f83f9b8e51fc22f82f9
Alexander Solodov <notdest@gmail.com>
2022-11-27 02:19:41 +0300

    Оформил рисовалку приземлений

--- includes/draw_angle.m
+++ includes/draw_angle.m
@@ -5,4 +5,7 @@ function draw_angle(X,Y,angle)
     y1  = Y - sin(angle)*len/2;
     y2  = Y + sin(angle)*len/2;
     line([x1,x2],[y1,y2],'linestyle','-','color','r');
+
+    angle   = -angle*180/pi;
+    text(x2+0.05,y2,strcat(num2str(angle,3),'°'));
 end
